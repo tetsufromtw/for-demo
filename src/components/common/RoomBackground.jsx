66f523e4ba9dd8d722b207a126a8f9783fce90c6
tetsufromtw <tetsufromtw@gmail.com>
2025-01-30 18:17:11 +0900

    update video path

--- src/components/common/RoomBackground.jsx
+++ src/components/common/RoomBackground.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import demoVideo from "../../assets/videos/demo.mp4";
+//import demoVideo from "../../assets/videos/demo.mp4";
 import demoImage from "../../assets/images/demo.png";
 import "./RoomBackground.css";
 
@@ -8,7 +8,8 @@ const RoomBackground = () => {
         <div className="room-background">
             <video
                 className="room-video"
-                src={demoVideo}
+                //src={demoVideo}
+                src="https://fordemo-gp.s3.us-east-1.amazonaws.com/demo.mp4"
                 autoPlay
                 loop
                 muted
