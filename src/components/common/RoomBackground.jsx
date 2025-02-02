9392e2c00fbf093a700d8a7d16764c75ab43c48a
tetsufromtw <tetsufromtw@gmail.com>
2025-02-02 20:24:02 +0900

    mobile 画面調整

--- src/components/common/RoomBackground.jsx
+++ src/components/common/RoomBackground.jsx
@@ -1,18 +1,28 @@
-import React from "react";
-//import demoVideo from "../../assets/videos/demo.mp4";
+import React, { useEffect, useRef } from "react";
 import demoImage from "../../assets/images/demo.png";
 import "./RoomBackground.css";
 
 const RoomBackground = () => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.play().catch((error) => {
+                console.error("自動播放失敗，需用戶手動播放", error);
+            });
+        }
+    }, []);
+
     return (
         <div className="room-background">
             <video
                 className="room-video"
-                //src={demoVideo}
+                ref={videoRef}
                 src="https://fordemo-gp.s3.us-east-1.amazonaws.com/demo.mp4"
                 autoPlay
                 loop
                 muted
+                playsinline
                 poster={demoImage}
             >
                 你的瀏覽器不支援播放此影片。
